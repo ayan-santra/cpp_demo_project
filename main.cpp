d1fd2e38c209dc9d99cac01829a05e453e2043c3
Subhojeet Sahoo <57010722+Alpha17-2@users.noreply.github.com>
2023-09-25 22:51:14 +0530

    used a function for adding two numbers

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int sum(int a,int b){
+    return a+b;
+}
 int main(){
     int a,b;
     cin>>a>>b;
-    int c = a+b;
+    int c = sum(a,b);
     printf("%d",c);
     return 0;
 }
