3ecd98e0ca8120146ad7a203d32d88d62b7ca086
ayan-santra <ayansantra1434@gmail.com>
2023-09-25 23:19:21 +0530

    add a new function (diff)

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int sum(int a,int b){
     return a+b;
 }
+int diff(int a, int b) {
+    return a-b;
+}
 int main(){
     int a,b;
     cin>>a>>b;
     int c = sum(a,b);
-    printf("%d",c);
+    int d= diff(a,b);
     return 0;
 }
